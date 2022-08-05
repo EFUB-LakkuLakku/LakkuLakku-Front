0b9c2f0cbbf17a387fccff9448673a7db5c7db30
Yoojin Shin <yjshin1003@naver.com>
2022-08-06 00:28:17 +0900

    feat: 속지 api 연결

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,7 +4,7 @@ import { BASE_URL } from "../constants/Config";
 const API = axios.create({
   baseURL: BASE_URL, // 기본 서버 주소 입력
   headers: {
-    Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ5ajEyMzRAZ21haWwuY29tIiwicm9sZXMiOiJVU0VSIiwiaWF0IjoxNjU5NjQ4MzY5LCJleHAiOjE2NTk2NTU1Njl9.eBrJxb6KDoQQOlq1LnnhWyeyrcnmhQK5EK7zDj1xtZU`
+    Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ5ajEyMzRAZ21haWwuY29tIiwicm9sZXMiOiJVU0VSIiwiaWF0IjoxNjU5NzA3MDk4LCJleHAiOjE2NTk3MTQyOTh9.-3snEMCpUH7XyIrebHidvZGPlwCQCocNtgZAm7MHokA`
   },
   withCredentials: false, // 이거 반드시 false 로 설정해야 서버에서 wildcard (*) 썼을때 오류 안나도록 할 수 있음
   crossDomain: true,
